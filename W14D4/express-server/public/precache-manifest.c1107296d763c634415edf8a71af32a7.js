d3ebd20e2027f0e61e872fdd100067425921f6a0
Nick Mallas <nmallas23@gmail.com>
2020-08-20 21:42:58 -0500

    Router Project

---
+++ W14D4/express-server/public/precache-manifest.c1107296d763c634415edf8a71af32a7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c925c1914aab056e3cf42fe53754dc03",
+    "url": "/index.html"
+  },
+  {
+    "revision": "39e5f2a865fbab35e1d4",
+    "url": "/static/css/main.d3fa6e51.chunk.css"
+  },
+  {
+    "revision": "b22c9ba60b0d609baba0",
+    "url": "/static/js/2.cf67a1cd.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.cf67a1cd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "39e5f2a865fbab35e1d4",
+    "url": "/static/js/main.a98f5b5f.chunk.js"
+  },
+  {
+    "revision": "256780cce990331d9974",
+    "url": "/static/js/runtime-main.83a27b56.js"
+  }
+]);
