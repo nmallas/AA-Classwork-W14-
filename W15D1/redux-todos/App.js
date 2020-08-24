6739d375f903a21764faaa6f897cb119295bdeda
Nick Mallas <nmallas23@gmail.com>
2020-08-24 17:15:47 -0500

    dispatching actions

---
+++ W15D1/redux-todos/App.js
@@ -0,0 +1,24 @@
+const {
+    store,
+    createTask,
+    deleteTask,
+    resetTaskList,
+  } = require('./reduxStoreActionReducer');
+
+
+console.log('Default Redux Store (empty task list):');
+console.log(store.getState())
+
+store.dispatch(createTask("Walk the dog"));
+store.dispatch(createTask('talk to bird'));
+store.dispatch(createTask('walk the goldfish'));
+store.dispatch(createTask("Make coffee"));
+
+console.log('Redux Store:');
+console.log(store.getState());
+
+store.dispatch(deleteTask(0));
+store.dispatch(deleteTask(0));
+
+console.log('Updated Redux Store:');
+console.log(store.getState())
--- W15D1/redux-todos/reduxStoreActionReducer.js
+++ W15D1/redux-todos/reduxStoreActionReducer.js
@@ -2,11 +2,21 @@ const {createStore} = require("redux");
 
 const CREATE_TASK = 'CREATE_TASK';
 const DELETE_TASK = 'DELETE_TASK';
+const RESET_TASK_LIST = "RESET_TASK_LIST"
 
 const taskReducer = (state = [], action) => {
     switch(action.type) {
-        case(""):
-            return;
+        case(CREATE_TASK):
+            const newTask = {
+                message: action.taskMessage,
+            };
+            return [...state, newTask];
+        case(DELETE_TASK):
+            return [...state.slice(0, action.taskId), ...state.slice(action.taskId + 1)];
+        case(RESET_TASK_LIST):
+            return action.emptyTaskList;
+        default:
+            return state;
     }
 }
 
@@ -24,4 +34,18 @@ const deleteTask = (taskId) => {
         type: DELETE_TASK,
         taskId
     }
+}
+
+const resetTaskerList = () => {
+    return {
+        type: RESET_TASK_LIST,
+        emptyTaskList: [],
+      };
+}
+
+module.exports = {
+    store,
+    createTask,
+    deleteTask,
+    resetTaskerList
 }
