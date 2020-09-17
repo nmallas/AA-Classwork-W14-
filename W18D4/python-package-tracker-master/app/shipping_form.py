34c1fd780f439d4218abe01ada11de9cb045ccea
Nick Mallas <nmallas23@gmail.com>
2020-09-17 16:57:26 -0500

    integrated alembic

--- W18D4/python-package-tracker-master/app/shipping_form.py
+++ W18D4/python-package-tracker-master/app/shipping_form.py
@@ -1,18 +1,24 @@
 from flask_wtf import FlaskForm
 from wtforms.fields import (
-  StringField, BooleanField, SelectField
+  StringField, BooleanField, SelectField, SubmitField
 )
 from wtforms.validators import (
-    DataRequired
+    DataRequired, AnyOf
 )
+
 from map.map import map
 
-origin_cities = [enumerate(keys(map))]
-print(origin_cities)
+origin_cities = map.keys()
+destination_cities = map.values()
+
 
 class ShippingForm(FlaskForm):
     sender = StringField("Sender Name", [DataRequired()])
-    recipient = StringField(u"Recipient Name", [DataRequired("Must add a recipient")])
-    origin = SelectField("Origin", [DataRequired("Must add an origin")])
-    destination = SelectField("Destination", [DataRequired("Must add a destination")])
+    recipient = StringField("Recipient Name", [DataRequired(
+      "Must add a recipient")])
+    origin = SelectField("Origin", [DataRequired(
+      "Must add an origin")], choices=origin_cities)
+    destination = SelectField("Destination", [DataRequired(
+      "Must add a destination")], choices=destination_cities)
     express_shipping = BooleanField("Express Shipping", default=False)
+    submit = SubmitField("Submit")
--- W18D4/python-package-tracker-master/package_tracker.py
+++ W18D4/python-package-tracker-master/package_tracker.py
@@ -1,11 +1,15 @@
 from flask import Flask, render_template
 from config import Configuration
+from app.shipping_form import ShippingForm
+from flask_migrate import Migrate
+from app.models import db
 # from app import routes
 # import os
 
-
 app = Flask(__name__)
 app.config.from_object(Configuration)
+db.init_app(app)
+migrate = Migrate(app, db)
 
 
 # app.config.update({'SECRET_KEY': os.environ.get('SECRET_KEY')})
@@ -18,4 +22,6 @@ def package_tracker():
 
 @app.route("/new_package", methods=["GET", "POST"])
 def new_package():
-    return render_template('shipping_request.html')
+
+    form = ShippingForm()
+    return render_template('shipping_request.html', form=form)
