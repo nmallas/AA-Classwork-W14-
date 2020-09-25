bef514e734f7c8495151417f6f5eebb4a2d688a4
Nick Mallas <nmallas23@gmail.com>
2020-09-25 16:37:13 -0500

    finish rating route

--- W19D5/microservices-python/app/__init__.py
+++ W19D5/microservices-python/app/__init__.py
@@ -1,9 +1,24 @@
-from flask import Flask;
-from app.models import db;
-from app.config import Configuration;
+from flask import Flask, jsonify
+from app.models import db, Rating
+from app.config import Configuration
 from flask_migrate import Migrate
 
 app = Flask(__name__)
 app.config.from_object(Configuration)
 db.init_app(app)
 migrate = Migrate(app, db)
+
+@app.route("/")
+def home():
+    return "<h1>FLASK APP WORKING!!!</h1>"
+
+@app.route("/ratings/<int:book_id>")
+def getRating(book_id):
+    ratings = Rating.query.filter(Rating.book_id == book_id).all()
+    rating_values = [rating.value for rating in ratings]
+    print(rating_values)
+    if not rating_values:
+        res = {"error": 'No ratings for this book yet.'}
+        return jsonify(res), 404
+    rating_average = round((sum(rating_values) / len(rating_values)), 2)
+    return jsonify({"avg": rating_average, "values": rating_values})
--- W19D5/microservices-python/book_ratings.py
+++ W19D5/microservices-python/book_ratings.py
@@ -1,5 +1,1 @@
 import app
-
-@app.route("/")
-def home():
-    return "<h1>FLASK APP WORKING!!!</h1>"
