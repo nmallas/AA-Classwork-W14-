a140d3e40ebb323a392d5d35990c2a37a732c101
Nick Mallas <nmallas23@gmail.com>
2020-10-19 22:09:02 -0500

    leetcode easy

--- algo_practice/morepractice_w22.js
+++ algo_practice/morepractice_w22.js
@@ -15,3 +15,36 @@ var missingNumber = function(nums) {
         if(!numsFound.has(i)) return i;
     }
 };
+
+
+// Leetcode #58
+
+var lengthOfLastWord = function(s) {
+    if(!s.trim()) return 0;
+    let words = s.split(" ").filter(word => word !== "");
+    return words[words.length -1].length
+};
+
+
+// Leetcode #67
+
+var addBinary = function(a, b) {
+    let aSplit = a.split("").reverse()
+    let bSplit = b.split("").reverse()
+    let newBString = [];
+    let carry = false;
+    for(let i=0; i<Math.max(a.length, b.length); i++) {
+        let newA = Number(aSplit[i]) || 0 ;
+        let newB = Number(bSplit[i]) || 0 ;
+        let add = carry ? 1 : 0;
+        newVal = newA + newB + add;
+        carry = false;
+        if(newVal >= 2) {
+            carry = true;
+            newVal -= 2;
+        }
+        newBString.push(newVal)
+    }
+    if(carry) newBString.push(1)
+    return newBString.reverse().join("")
+};
