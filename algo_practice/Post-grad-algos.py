029b954f4e50bd48a28102023bd9c6e36036c00e
Nick Mallas <nmallas23@gmail.com>
2020-11-24 16:46:13 -0800

    leetcode 345

--- algo_practice/Post-grad-algos.py
+++ algo_practice/Post-grad-algos.py
@@ -102,3 +102,26 @@ class Solution:
 
         inOrder(root, values)
         return values[k-1]
+
+
+# Leetcode 345
+# Time complexity O(n)
+# Space complexity O(n)
+
+class Solution:
+    def reverseVowels(self, s: str) -> str:
+        vowels = "aeiouAEIOU"
+        left = 0
+        right = len(s) -1
+        chars = list(s)
+        while left < right:
+            if chars[left] in vowels:
+                if chars[right] in vowels:
+                    chars[left], chars[right] = chars[right], chars[left]
+                    left += 1
+                    right -= 1
+                else:
+                    right -= 1
+            else:
+                left += 1
+        return "".join(chars)
